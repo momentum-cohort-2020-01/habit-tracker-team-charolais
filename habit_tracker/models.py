97e13d8960877b48470753aa3ab67bcd9d6bf444
aselkg <aselekkg@gmail.com>
2020-03-05 17:47:08 -0500

    little changes in model and forms

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Habit(models.Model):
+    record = models.ForeignKey('HabitRecord', on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=50)
-    goal = models.CharField(max_length=80)
+    goal = models.IntegerField()
+    unit = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+
     
     def __str__ (self):
         return f'name: {self.name} goal: {self.goal}'
 
 class HabitRecord(models.Model):
-    #habit =  #foreign key to connect habits
     progress = models.IntegerField()
     updated_at = models.DateTimeField(auto_now=True)
